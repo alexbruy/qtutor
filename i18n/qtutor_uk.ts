d6d52406ee116f6d6fca520c6cbbb78ea0e8eb30
Alexander Bruy <alexander.bruy@gmail.com>
2018-10-26 13:42:29 +0300

    add Ukrainian localization

---
+++ i18n/qtutor_uk.ts
@@ -0,0 +1,254 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../gui/aboutdialog.py" line="57"/>
+        <source>About {}</source>
+        <translation>Про {}</translation>
+    </message>
+    <message>
+        <location filename="../gui/aboutdialog.py" line="61"/>
+        <source>Version: {}</source>
+        <translation>Версія: {}</translation>
+    </message>
+    <message>
+        <location filename="../gui/aboutdialog.py" line="63"/>
+        <source>&lt;p&gt;{description}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: {developer}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;{homepage}&quot;&gt;{homepage}&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;{bugtracker}&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;{description}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Розробники&lt;/strong&gt;: {developer}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;ДОмашня сторінка&lt;/strong&gt;: &lt;a href=&quot;{homepage}&quot;&gt;{homepage}&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Про помилки повідомляйте у &lt;a href=&quot;{bugtracker}&quot;&gt;багтрекері&lt;/a&gt;.&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="40"/>
+        <source>TextLabel</source>
+        <translation>TextLabel</translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="63"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutorfinisheddialogbase.ui" line="14"/>
+        <source>Lesson finished</source>
+        <translation>Урок закінчено</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutorlibrarydialogbase.ui" line="14"/>
+        <source>QTutor library</source>
+        <translation>Бібліотека QTutor</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutorlibrarydialogbase.ui" line="56"/>
+        <source>Add lessons…</source>
+        <translation>Додати уроки…</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutorlibrarydialogbase.ui" line="63"/>
+        <source>Remove lesson(s)</source>
+        <translation>Вилучити урок(и)</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutorlibrarydialogbase.ui" line="83"/>
+        <source>Start lesson</source>
+        <translation>Почати урок</translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../ui/qtutoroptionswidgetbase.ui" line="14"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutoroptionswidgetbase.ui" line="20"/>
+        <source>QTutor paths</source>
+        <translation>Шляхи пошуку QTutor</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutoroptionswidgetbase.ui" line="26"/>
+        <source>Path(s) to search for QTutor lessons</source>
+        <translation>Шукати уроки QTutor у наступних каталогах</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutoroptionswidgetbase.ui" line="53"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutoroptionswidgetbase.ui" line="66"/>
+        <source>Don&apos;t load built-in (demo) lessons</source>
+        <translation>Не завантажувати вбудовані (демонстраційні) уроки</translation>
+    </message>
+</context>
+<context>
+    <name>Lesson</name>
+    <message>
+        <location filename="../lesson.py" line="112"/>
+        <source>Open project</source>
+        <translation>Відкрити проект</translation>
+    </message>
+    <message>
+        <location filename="../lesson.py" line="112"/>
+        <source>Open project with lesson data.</source>
+        <translation>Відкрити проект з даними уроку.</translation>
+    </message>
+    <message>
+        <location filename="../lesson.py" line="145"/>
+        <source>Can not find menu &quot;{}&quot;.</source>
+        <translation>Не вдалося знайти меню «{}».</translation>
+    </message>
+    <message>
+        <location filename="../lesson.py" line="270"/>
+        <source>Can not load lesson from {}:
+{}</source>
+        <translation>Не вдалося завантажити урок з {}:
+{}</translation>
+    </message>
+</context>
+<context>
+    <name>QTutorDock</name>
+    <message>
+        <location filename="../ui/qtutordockbase.ui" line="14"/>
+        <source>&amp;QTutor</source>
+        <translation>&amp;QTutor</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutordockbase.ui" line="21"/>
+        <source>TextLabel</source>
+        <translation>TextLabel</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutordock.py" line="189"/>
+        <source>Next</source>
+        <translation>Далі</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutordockbase.ui" line="63"/>
+        <source>Execute</source>
+        <translation>Виконати</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutordockbase.ui" line="83"/>
+        <source>Restart</source>
+        <translation>Перезапустити</translation>
+    </message>
+    <message>
+        <location filename="../ui/qtutordockbase.ui" line="90"/>
+        <source>Quit</source>
+        <translation>Вийти</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutordock.py" line="91"/>
+        <source>Looks like step was not completed. Please complete all instructions and try again.</source>
+        <translation>Здається, ви не виконали всі необхідні дії. Будь ласка, переконайтеся, що всі інструкції виконано та спробуйте ще раз.</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutordock.py" line="93"/>
+        <source>QTutor</source>
+        <translation>QTutor</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutordock.py" line="124"/>
+        <source>Lesson is not completed!</source>
+        <translation>Урок не закінчено!</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutordock.py" line="124"/>
+        <source>Current lesson is not completed. Do you want to finish it and return to the QTutor library?</source>
+        <translation>Поточний урок не закінчено. Завершити його та повернутися до бібліотеки QTutor?</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutordock.py" line="147"/>
+        <source>Finish</source>
+        <translation>Завершити</translation>
+    </message>
+</context>
+<context>
+    <name>QTutorFinishedDialog</name>
+    <message>
+        <location filename="../gui/qtutorfinisheddialog.py" line="49"/>
+        <source>&lt;p&gt;&lt;strong&gt;Congratulations! You have successfully finished this lesson.&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;Close this dialog to go back to the lessons library.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;strong&gt;Вітаємо! Ви успішно виконали урок.&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;Закрийте це вікно, щоб перейти до бібліотеки QTutor.&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutorfinisheddialog.py" line="61"/>
+        <source>&lt;p&gt;&lt;strong&gt;Congratulations! You have successfully finished this lesson.&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;The lesson&apos;s author(s) also recommend to complete following lessons:&lt;/p&gt;</source>
+        <translation>&lt;p&gt;&lt;strong&gt;Вітаємо! Ви успішно виконали урок.&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;Автор(и) уроку рекомендують також виконати наступні уроки:&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutorfinisheddialog.py" line="63"/>
+        <source>You can either close this dialog and go back to the lessons library or select one of the recommended lessons.</source>
+        <translation>Ви можете закрити цей діалог та повернутися до бібліотеки QTutor або ж вибрати один з рекомендованих уроків.</translation>
+    </message>
+</context>
+<context>
+    <name>QTutorLibraryDialog</name>
+    <message>
+        <location filename="../gui/qtutorlibrarydialog.py" line="79"/>
+        <source>Select file</source>
+        <translation>Виберіть файл</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutorlibrarydialog.py" line="79"/>
+        <source>ZIP archives (*.zip *.ZIP)</source>
+        <translation>Архіви ZIP (*.zip *.ZIP)</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutorlibrarydialog.py" line="98"/>
+        <source>Lesson already running!</source>
+        <translation>Вже є активний урок!</translation>
+    </message>
+    <message>
+        <location filename="../gui/qtutorlibrarydialog.py" line="98"/>
+        <source>Can not start lesson as there is already running one. Please finish active lesson and try again.</source>
+        <translation>Не вдалося розпочати урок, оскільки вже запущено інший. Будь ласка, завершіть активний урок та спробуйте ще раз.</translation>
+    </message>
+</context>
+<context>
+    <name>QTutorOptionsWidget</name>
+    <message>
+        <location filename="../gui/qtutoroptionswidget.py" line="84"/>
+        <source>Choose a directory</source>
+        <translation>Виберіть каталог</translation>
+    </message>
+</context>
+<context>
+    <name>QTutorPlugin</name>
+    <message>
+        <location filename="../qtutorplugin.py" line="93"/>
+        <source>QTutor</source>
+        <translation>QTutor</translation>
+    </message>
+    <message>
+        <location filename="../qtutorplugin.py" line="78"/>
+        <source>About QTutorâ¦</source>
+        <translation>Про QTutor…</translation>
+    </message>
+</context>
+<context>
+    <name>QTutorRegistry</name>
+    <message>
+        <location filename="../qtutorregistry.py" line="109"/>
+        <source>Lesson &quot;{}&quot; is not a user lesson and can not be uninstalled.</source>
+        <translation>Урок «{}» не встановлений користувачем і не може бути вилучений.</translation>
+    </message>
+    <message>
+        <location filename="../qtutorregistry.py" line="132"/>
+        <source>Duplicate lesson name &quot;{}&quot; for group &quot;{}&quot;</source>
+        <translation>Дублюється назва уроку «{}» у групі «{}»</translation>
+    </message>
+</context>
+</TS>
